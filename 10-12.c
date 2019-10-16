c1b91ce8aff7f9680e4d7e74cf01ae9694b11895
Daisy <Daisy@localhost.localdomain>
2019-10-16 22:28:02 +0800

    简单的shell

---
+++ 10-12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+	pid_t id=fork();
+	if(id<0)
+	{
+		perror("fork");
+	}
+	else if(id==0)//child
+	{
+		printf("I am a child : pid:%d ppid:%d\n",getpid(),getppid());
+	}
+	else
+		printf("I am a parent : pid:%d ppid:%d\n",getpid(),getppid());
+	sleep(1);
+}
---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#一个僵尸进程的例子
+int main()
+{
+	pid_t id=fork();
+	if(id<0)
+	{
+		perror("fork");
+		return 1;
+	}
+	else if(id>0)
+	{
+		printf("parent[%d] is sleeping...\n",getpid());
+		sleep(30);
+	}
+	else
+	{
+		printf("child[%d] is begin Z...\n",getpid());
+		sleep(5);
+		exit(EXIT_SUCCESS);
+	}
+	return 0;
+}
